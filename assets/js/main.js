df340b020bea0c58b023fb4e4d47ab7f37a79871
Mahir Hasanli <126595932+MAHIRHASANLI@users.noreply.github.com>
2025-01-16 19:43:59 +0530

    updated home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,33 @@
+
+// Şəkillərin olduğu massiv
+const images = [
+     'url("../../images/01.jpg")',
+     'url("../../images/03.jpeg")',
+     'url("../../images/02.jpeg")'
+];
+
+
+// Başlanğıc olaraq birinci şəkli göstərək
+let currentIndex = 0;
+
+const bodyElement = document.body;
+
+// Arxa fon şəklini dəyişdirən funksiyanı yazırıq
+function changeBackground() {
+     // Növbəti şəkli təyin edirik
+     currentIndex = (currentIndex + 1) % images.length;
+
+     // Yeni fon şəklini body-yə təyin edirik
+     bodyElement.style.backgroundImage = images[currentIndex];
+}
+
+// İlk fon şəklini təyin edək
+bodyElement.style.backgroundImage = images[currentIndex];
+
+// Hər 5 dəqiqədən bir şəkli dəyişdirən interval
+setInterval(changeBackground, 5000);
+
+
 const menuLink = document.querySelectorAll(".menu");
 
 menuLink.forEach((item) => {
@@ -12,3 +42,4 @@ menuLink.forEach((item) => {
      });
 
 })
+
