d2e48aa349f9ecb80061d2622846f39ef3032693
Mahir Hasanli <126595932+MAHIRHASANLI@users.noreply.github.com>
2025-01-17 17:40:45 +0530

    completed home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,46 +1,74 @@
-
-// Şəkillərin olduğu massiv
-const images = [
-     'url("../../images/01.jpg")',
-     'url("../../images/03.jpeg")',
-     'url("../../images/02.jpeg")'
-];
-
-
-// Başlanğıc olaraq birinci şəkli göstərək
-let currentIndex = 0;
-
-const bodyElement = document.body;
-
-// Arxa fon şəklini dəyişdirən funksiyanı yazırıq
-function changeBackground() {
-     // Növbəti şəkli təyin edirik
-     currentIndex = (currentIndex + 1) % images.length;
-
-     // Yeni fon şəklini body-yə təyin edirik
-     bodyElement.style.backgroundImage = images[currentIndex];
+allFunctions()
+function allFunctions() {
+     addPageSlider();
+     openAccardion()
+     setupScrollButton()
 }
 
-// İlk fon şəklini təyin edək
-bodyElement.style.backgroundImage = images[currentIndex];
 
-// Hər 5 dəqiqədən bir şəkli dəyişdirən interval
-setInterval(changeBackground, 10000);
+//! ANA SEHIFE DLIDER I
+function addPageSlider() {
+     // Şəkillərin olduğu massiv
+     const images = [
+          'url("../../images/01.jpg")',
+          'url("../../images/03.jpeg")',
+          'url("../../images/02.jpeg")'
+     ];
 
+     // Başlanğıc olaraq birinci şəkli göstərək
+     let currentIndex = 0;
 
-const menuLink = document.querySelectorAll(".menu");
-menuLink.forEach((link) => {
-     link.addEventListener("click", function () {
-          const accardions = document.querySelectorAll(".accardion")
-          accardions.forEach((accardion) => {
-               if (link.nextElementSibling === accardion) {
-                    accardion.style.height = (accardion.children.length * 35) + "px";
-               }
-               else {
-                    accardion.style.height = "0px";
-               }
-          })
+     const bodyElement = document.body;
+
+     // Arxa fon şəklini dəyişdirən funksiyanı yazırıq
+     function changeBackground() {
+          // Növbəti şəkli təyin edirik
+          currentIndex = (currentIndex + 1) % images.length;
+
+          // Yeni fon şəklini body-yə təyin edirik
+          bodyElement.style.backgroundImage = images[currentIndex];
+     }
+
+     // İlk fon şəklini təyin edək
+     bodyElement.style.backgroundImage = images[currentIndex];
+
+     // Hər 5 dəqiqədən bir şəkli dəyişdirən interval
+     setInterval(changeBackground, 10000);
+}
+
+//! ACCARDION LINK
+function openAccardion() {
+     const menuLink = document.querySelectorAll(".menu");
+     menuLink.forEach((link) => {
+          link.addEventListener("click", function () {
+               const accardions = document.querySelectorAll(".accardion")
+               accardions.forEach((accardion) => {
+                    if (link.nextElementSibling === accardion) {
+                         accardion.style.height = (accardion.children.length * 35) + "px";
+                    }
+                    else {
+                         accardion.style.height = "0px";
+                    }
+               })
+          });
+
+     })
+}
+
+//! BUTTON SCROLL ILE GERI DONME
+function setupScrollButton() {
+     const backBtn = document.querySelector(".top-back-btn");
+     window.addEventListener("scroll", function () {
+          if (this.scrollY > 50) {
+               backBtn.style.display = "block";
+
+          } else {
+               backBtn.style.display = "none";
+          }
      });
 
-})
+     backBtn.addEventListener("click", function () {
+          window.scrollTo({ top: 0, behavior: "smooth" });
+     })
 
+}
